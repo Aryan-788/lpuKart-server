9adfdf5c84a1abf6a5a5611022a530ecd652a550
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2025-03-31 23:35:27 +0530

    Added Cors links in index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(passport.initialize())
 passport.use("jwt",jwtStrategy)
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://lpukart.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
